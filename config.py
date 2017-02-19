c16d909230ec07556efee699f4bc879b1c8beb96
Denis Panin <denis.a.panin@gmail.com>
2017-02-19 18:55:18 +0300

    Hide SQLALCHEMY_TRACK_MODIFICATIONS warning

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
