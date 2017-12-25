8b7568cfcf58e6cbf15391c179f0b5dc6199d198
Denis Panin <denis.a.panin@gmail.com>
2017-12-25 22:36:25 +0300

    Auto add statuses on database creation

--- setup_db.py
+++ setup_db.py
@@ -3,6 +3,12 @@ from app import db, models
 
 db.create_all()
 
+statuses = ['Заявление зарегистрировано', 'Акцизные марки изготовлены', 'Обязательство принято', 'Обеспечение принято',
+            'Марки получены', 'Отчет принят']
+
+for status in statuses:
+    db.session.add(models.Status(name=status))
+
 username = argv[1]
 email = argv[2]
 password = argv[3]
